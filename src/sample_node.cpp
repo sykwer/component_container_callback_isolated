76e0e34b726c9b0f101ab62430a1b53d98150f4f
Takahiro Ishikawa <sykwer@gmail.com>
2023-09-06 14:55:13 +0900

    add skelton

--- src/sample_node.cpp
+++ src/sample_node.cpp
@@ -4,6 +4,51 @@
 #include <vector>
 #include "rclcpp/rclcpp.hpp"
 
+// StaticCallbackDedicatedMultiThreadedExecutor - from here
+class StaticCallbackDedicatedMultiThreadedExecutor : public rclcpp::Executor {
+public:
+  RCLCPP_SMART_PTR_DEFINITIONS(StaticCallbackDedicatedMultiThreadedExecutor)
+
+  RCLCPP_PUBLIC
+  explicit StaticCallbackDedicatedMultiThreadedExecutor(
+      const rclcpp::ExecutorOptions &options = rclcpp::ExecutorOptions());
+
+  RCLCPP_PUBLIC
+  virtual ~StaticCallbackDedicatedMultiThreadedExecutor();
+
+  RCLCPP_PUBLIC
+  void spin() override;
+
+  RCLCPP_PUBLIC
+  void add_node(rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node, bool notify = true) override;
+
+  RCLCPP_PUBLIC
+  void add_node(std::shared_ptr<rclcpp::Node> node, bool notify = true) override;
+};
+
+StaticCallbackDedicatedMultiThreadedExecutor::StaticCallbackDedicatedMultiThreadedExecutor(
+    const rclcpp::ExecutorOptions &options) : rclcpp::Executor(options) {
+}
+
+StaticCallbackDedicatedMultiThreadedExecutor::~StaticCallbackDedicatedMultiThreadedExecutor() {
+}
+
+void StaticCallbackDedicatedMultiThreadedExecutor::spin() {
+}
+
+void StaticCallbackDedicatedMultiThreadedExecutor::add_node(
+    rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node, bool notify) {
+  (void) node;
+  (void) notify;
+}
+
+void StaticCallbackDedicatedMultiThreadedExecutor::add_node(
+    std::shared_ptr<rclcpp::Node> node, bool notify) {
+  this->add_node(node->get_node_base_interface(), notify);
+}
+
+// StaticCallbackDedicatedMultiThreadedExecutor - to here
+
 #include "interfaces/msg/dynamic_size_array.hpp"
 
 using namespace std::chrono_literals;
@@ -42,7 +87,13 @@ private:
 
 int main(int argc, char * argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<SampleNode>());
+
+  auto node = std::make_shared<SampleNode>();
+  auto executor = std::make_shared<StaticCallbackDedicatedMultiThreadedExecutor>();
+
+  executor->add_node(node);
+  executor->spin();
+
   rclcpp::shutdown();
   return 0;
 }
