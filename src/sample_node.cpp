b2a21a57ad2e08ba77d9eedc1153d0301f614682
Takahiro Ishikawa <sykwer@gmail.com>
2023-09-07 19:52:02 +0900

    revert

--- src/sample_node.cpp
+++ src/sample_node.cpp
@@ -4,51 +4,6 @@
 #include <vector>
 #include "rclcpp/rclcpp.hpp"
 
-// StaticCallbackDedicatedMultiThreadedExecutor - from here
-class StaticCallbackDedicatedMultiThreadedExecutor : public rclcpp::Executor {
-public:
-  RCLCPP_SMART_PTR_DEFINITIONS(StaticCallbackDedicatedMultiThreadedExecutor)
-
-  RCLCPP_PUBLIC
-  explicit StaticCallbackDedicatedMultiThreadedExecutor(
-      const rclcpp::ExecutorOptions &options = rclcpp::ExecutorOptions());
-
-  RCLCPP_PUBLIC
-  virtual ~StaticCallbackDedicatedMultiThreadedExecutor();
-
-  RCLCPP_PUBLIC
-  void spin() override;
-
-  RCLCPP_PUBLIC
-  void add_node(rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node, bool notify = true) override;
-
-  RCLCPP_PUBLIC
-  void add_node(std::shared_ptr<rclcpp::Node> node, bool notify = true) override;
-};
-
-StaticCallbackDedicatedMultiThreadedExecutor::StaticCallbackDedicatedMultiThreadedExecutor(
-    const rclcpp::ExecutorOptions &options) : rclcpp::Executor(options) {
-}
-
-StaticCallbackDedicatedMultiThreadedExecutor::~StaticCallbackDedicatedMultiThreadedExecutor() {
-}
-
-void StaticCallbackDedicatedMultiThreadedExecutor::spin() {
-}
-
-void StaticCallbackDedicatedMultiThreadedExecutor::add_node(
-    rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node, bool notify) {
-  (void) node;
-  (void) notify;
-}
-
-void StaticCallbackDedicatedMultiThreadedExecutor::add_node(
-    std::shared_ptr<rclcpp::Node> node, bool notify) {
-  this->add_node(node->get_node_base_interface(), notify);
-}
-
-// StaticCallbackDedicatedMultiThreadedExecutor - to here
-
 #include "interfaces/msg/dynamic_size_array.hpp"
 
 using namespace std::chrono_literals;
@@ -89,7 +44,7 @@ int main(int argc, char * argv[]) {
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<SampleNode>();
-  auto executor = std::make_shared<StaticCallbackDedicatedMultiThreadedExecutor>();
+  auto executor = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
 
   executor->add_node(node);
   executor->spin();
