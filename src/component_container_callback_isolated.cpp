cde4ea222322d81bb84965ed9d8027a2da539afc
Takahiro Ishikawa <sykwer@gmail.com>
2023-09-08 01:52:53 +0900

    add skelton

---
+++ src/component_container_callback_isolated.cpp
@@ -0,0 +1,15 @@
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_components/component_manager.hpp"
+
+namespace rclcpp_components
+{
+
+class ComponentContainerCallbackIsolated : rclcpp_components::ComponentManager {
+
+};
+
+} // rclcpp_components
+
+int main(int argc, char *argv[]) {
+
+}
